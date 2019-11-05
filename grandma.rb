df6c757fcd8f42bb59da00700451bb4ba06e07f5
Sean Good <tree662@protonmail.com>
2019-11-05 22:12:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,16 +1,3 @@
-def speak_to_grandma
-  if speak_to_grandma == "I LOVE YOU GRANDMA!" 
-    puts "I LOVE YOU TOO PUMPKIN!"
-  elsif speak_to_grandma == "Hi Nana, how are you?" 
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif speak_to_grandma == "Hi!" 
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?" 
-    puts "NO, NOT SINCE 1938!"
-  else speak_to_grandma == "WHAT?" 
-    puts "NO, NOT SINCE 1938!"
-end
-
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
  		return "I LOVE YOU TOO PUMPKIN!"
